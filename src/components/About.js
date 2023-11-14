a31db57f50e35c3d3f8fde14d77f0abb31fcb868
bilalcouldnever <bilal.faruq@student.moringaschool.com>
2023-11-14 18:22:13 +0300

    done

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,14 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
-}
-
+  return (
+    <div id="about">
+      <h2>About Me</h2>
+      <p>Hi, I'm Bilal, a software based in Nairobi. With a passion for coding and problem-solving,
+        I enjoy working on diverse software projects and contributing to the world of technology.
+        Let's build something amazing together!</p>
+      <img src={image} alt="I made this" />
+    </div>
+  );
+};
 export default About;
